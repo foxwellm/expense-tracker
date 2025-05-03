f8211fcafa819649be7f62cb93d1940a857a1a95
Matthew Foxwell <foxwellm@gmail.com>
2025-05-03 00:38:48 -0700

    feat: add material-ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,18 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Roboto } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// NOTE: Prevents Flash of Unstyled Content (FOUC)
+// https://mui.com/material-ui/integrations/nextjs/#app-router
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "../theme";
+
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
   subsets: ["latin"],
+  display: "swap",
+  variable: "--font-roboto",
 });
 
 export const metadata: Metadata = {
@@ -23,9 +26,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+    <html lang="en" className={roboto.variable}>
+      <body>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        </AppRouterCacheProvider>
       </body>
     </html>
   );
