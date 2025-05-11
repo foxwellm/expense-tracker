9dad92b5de68698184a5b5811250c88f34514c77
Matthew Foxwell <foxwellm@gmail.com>
2025-05-11 02:29:57 -0700

    feat: parse expense api

--- src/app/api/expenses/route.ts
+++ src/app/api/expenses/route.ts
@@ -1,6 +1,30 @@
 import { NextRequest, NextResponse } from 'next/server'
+import { z } from 'zod'
 
 import { createServerSupabaseClient } from '@/utils/supabase'
+// import sanitizeHtml from 'sanitize-html'
+
+// NOTE: Removing an expenseCategory must be handles in the database by renaming the category or removing those rows
+const expenseCategories = ['Clothing', 'Food', 'Fuel', 'Entertainment'] as const
+
+const expenseSchema = z.object({
+  date: z.string().date(), // yyyy-mm-dd
+  expense: z.enum(expenseCategories),
+  cost: z.number().positive(),
+  // name: z.string().max(100),
+})
+
+const expensesSchema = z.array(expenseSchema)
+
+const sanitizeInput = (input: z.infer<typeof expenseSchema>) => {
+  return {
+    ...input,
+    // name: sanitizeHtml(input.name, {
+    //   allowedTags: [],
+    //   allowedAttributes: {},
+    // }),
+  }
+}
 
 export async function POST(request: NextRequest) {
   const supabase = await createServerSupabaseClient()
@@ -14,27 +38,26 @@ export async function POST(request: NextRequest) {
   }
 
   const body = await request.json()
-  const { date, expense, cost } = body
 
-  if (!date || !expense || typeof cost !== 'number') {
+  let parsedExpenses
+  try {
+    parsedExpenses = expensesSchema.parse(body)
+  } catch (err) {
     return NextResponse.json(
-      { error: 'Missing or invalid fields.' },
+      { error: 'Invalid request data', details: err },
       { status: 400 }
     )
   }
 
+  const cleanedExpenses = parsedExpenses.map((expense) => ({
+    ...sanitizeInput(expense),
+    user_id: user.id,
+  }))
+
   const { data, error } = await supabase
     .from('expenses')
-    .insert([
-      {
-        user_id: user.id,
-        date,
-        expense,
-        cost,
-      },
-    ])
+    .insert(cleanedExpenses)
     .select()
-    .single()
 
   if (error) {
     console.error('Supabase insert error:', error)
