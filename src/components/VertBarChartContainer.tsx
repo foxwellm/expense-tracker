306eb00e6e2485cf375c1dae1f7713dda97df76c
Matthew Foxwell <foxwellm@gmail.com>
2025-05-13 11:01:31 -0700

    feat: chart loading skeleton

--- src/components/VertBarChartContainer.tsx
+++ src/components/VertBarChartContainer.tsx
@@ -1,11 +1,15 @@
 'use client'
 
+import { Box, Skeleton } from '@mui/material'
 import { useEffect, useState } from 'react'
+
+import { CombinedMonthlyExpenses } from '@/types/expense'
 
 import { VertBarChart } from './VertBarChart'
 
 export function VertBarChartContainer() {
-  const [combinedExpenses, setCombinedExpenses] = useState()
+  const [combinedExpenses, setCombinedExpenses] =
+    useState<CombinedMonthlyExpenses>()
   useEffect(() => {
     const getMonthlyExpenses = async () => {
       const res = await fetch('/api/expenses')
@@ -15,21 +19,28 @@ export function VertBarChartContainer() {
     getMonthlyExpenses()
   }, [])
 
-  if (!combinedExpenses) {
-    return <></>
-  }
-
-  const { monthlyExpenses, categories, monthYearDomain } = combinedExpenses
   return (
-    <>
-      {combinedExpenses && (
+    <Box
+      sx={{
+        position: 'relative',
+        width: '100%',
+        aspectRatio: '16 / 9',
+      }}
+    >
+      {!combinedExpenses ? (
+        <Skeleton
+          variant="rectangular"
+          width="100%"
+          height="100%"
+          sx={{ position: 'absolute', top: 0, left: 0 }}
+        />
+      ) : (
         <VertBarChart
-          monthlyExpenses={monthlyExpenses}
-          categories={categories}
-          // TODO: Build function to construct domain from data
-          monthYearDomain={monthYearDomain}
+          monthlyExpenses={combinedExpenses.monthlyExpenses}
+          categories={combinedExpenses.categories}
+          monthYearDomain={combinedExpenses.monthYearDomain}
         />
       )}
-    </>
+    </Box>
   )
 }
