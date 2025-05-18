7648be5e5ce20d17891b577241d05b55c839e157
Matthew Foxwell <foxwellm@gmail.com>
2025-05-18 05:15:21 -0700

    fix: tsc on staged files only

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -5,16 +5,16 @@ const buildEslintCommand = (filenames) => {
     .map((f) => path.relative(process.cwd(), f))
     .join(' --file ')
   return [
-    'tsc --noEmit',
     `next lint --config ./eslint.config.mjs --fix --file ${files}`,
     `prettier --write ${filenames.join(' ')}`,
   ]
 }
 
 const lintStagedConfig = {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
-  '*.{json,md,mjs,html}': ['prettier --write'],
-  '*.test.ts': ['jest'],
+  '**/*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  '**/*.{ts,tsx}': ['tsc-files --noEmit'],
+  '**/*.{json,md,mjs,html}': ['prettier --write'],
+  '**/*.test.ts': ['jest'],
 }
 
 export default lintStagedConfig
