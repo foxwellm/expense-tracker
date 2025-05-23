8fbd1d3f40380237d4ecf46362e11ab978088631
Matthew Foxwell <foxwellm@gmail.com>
2025-05-23 06:42:29 -0700

    feat: dynamic header sizes

--- src/app/_hooks/useBreakpoint.ts
+++ src/app/_hooks/useBreakpoint.ts
@@ -2,10 +2,33 @@
 
 import { useMediaQuery, useTheme } from '@mui/material'
 
+const headers = {
+  h1: 'h1',
+  h2: 'h2',
+  h3: 'h3',
+  h4: 'h4',
+  h5: 'h5',
+  h6: 'h6',
+} as const
+
+type HeaderLevel = (typeof headers)[keyof typeof headers]
+
 export const useBreakpoint = () => {
   const theme = useTheme()
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'), { noSsr: true })
   const isTablet = useMediaQuery(theme.breakpoints.down('md'), { noSsr: true })
 
-  return { isMobile, isTablet, navHeight: isMobile ? 56 : 64 }
+  const navHeight = isMobile ? 56 : 64
+  const largeHeader: HeaderLevel = isMobile
+    ? headers.h5
+    : isTablet
+      ? headers.h2
+      : headers.h1
+  const mediumHeader: HeaderLevel = isMobile
+    ? headers.h6
+    : isTablet
+      ? headers.h4
+      : headers.h3
+
+  return { isMobile, isTablet, navHeight, largeHeader, mediumHeader }
 }
--- src/theme.ts
+++ src/theme.ts
@@ -27,9 +27,29 @@ export const getTheme = (mode: 'light' | 'dark') =>
     },
     typography: {
       fontFamily: 'Roboto, sans-serif',
-      h6: {
-        fontSize: '1.1rem',
+      h1: {
+        fontSize: '1.75rem',
+        fontWeight: 700,
+      },
+      h2: {
+        fontSize: '1.5rem',
+        fontWeight: 700,
+      },
+      h3: {
+        fontSize: '1.25rem',
         fontWeight: 500,
+      },
+      h4: {
+        fontSize: '1rem',
+        fontWeight: 500,
+      },
+      h5: {
+        fontSize: '0.875rem',
+        fontWeight: 500,
+      },
+      h6: {
+        fontSize: '0.75rem',
+        fontWeight: 400,
       },
     },
     components: {
